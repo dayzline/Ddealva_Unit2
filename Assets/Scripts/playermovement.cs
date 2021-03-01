18e0c798770ada38991cc84e8b94c2dd93f0a6d1
dayzline <69827441+dayzline@users.noreply.github.com>
2021-02-28 21:06:42 -0800

    Initial commit

---
+++ Assets/Scripts/playermovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+    private float horizontalInput;
+    private float xRange = 20f;
+
+    public float speed;
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+    }
+}
