a8b7c67086bb050b6ddd2ef5bd8cf03ea0a9f835
dayzline <69827441+dayzline@users.noreply.github.com>
2021-03-05 21:26:17 -0800

    Finished Lesson C

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    private float xrange = 20f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            float randXPos = Random.Range(-xrange, xrange);
+            int animalPrefabIndex = Random.Range(0, animalPrefabs.Length);
+            Vector3 randXPlacement = new Vector3(randXPos, 0, 30);
+            Instantiate(animalPrefabs[animalPrefabIndex], randXPlacement, animalPrefabs[animalPrefabIndex].transform.rotation);
+        }
+    }
+}
